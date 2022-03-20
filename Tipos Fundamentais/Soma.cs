cb0e20009d1f81f34d94f1f44ae0575093005677
thaismartinsz <thaismtinocotmgmail.com>
2022-03-20 20:41:35 -0300

    Soma

---
+++ Tipos Fundamentais/Soma.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LeraningProject 
+{
+    internal class Soma
+    {
+        static void Main(string[] args)
+        {
+            double num01;
+            double num02; 
+            double num03;
+
+            Console.Write("Insira um numero: ");
+
+            num01 = Convert.ToDouble( Console.ReadLine() );
+
+            Console.Write ("Coloque o segundo numero ");
+            num02 = Convert.ToDouble( Console.ReadLine() );
+
+            Console.Write("Agora, o terceiro numero. ");
+            num03 = Convert.ToDouble( Console.ReadLine());
+
+            double result = (num01 + num02 + num03) /3; 
+            Console.WriteLine("Esse é o resultado " + result);
+
+            Console.ReadKey();
+        }
+    }
+}
