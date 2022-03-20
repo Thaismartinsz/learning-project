39b8c55090b4944c88b5667a1aa4eaf58e999ecd
thaismartinsz <thaismtinocotmgmail.com>
2022-03-20 20:43:55 -0300

    Contas

---
+++ Condicionais/Contas.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace LearningProject
+{
+    class Contas
+    {
+        static void Main(string[] args)
+        {
+            int answer;
+
+            Console.Write("15 + 2 + 7 + 5 = ");
+
+            answer = Convert.ToInt32(Console.ReadLine());
+
+            if (answer == 25)
+            {
+                Console.WriteLine("Correto! ");
+            }
+            else
+            { 
+                Console.WriteLine("Errou! ");
+            }
+
+            Console.Write("15 + 12 + 28 + 5 = ");
+            answer = Convert.ToInt32(Console.ReadLine());
+
+            if (answer == 60)
+            {
+                Console.WriteLine("Correto! ");
+            }
+            else
+            {
+                Console.WriteLine("Errou! ");
+            }
+
+            Console.Write("15 + 3 * 28 = ");
+            answer = Convert.ToInt32(Console.ReadLine());
+
+            if (answer == 99)
+            {
+                Console.WriteLine("Correto! ");
+            }
+            else
+            {
+                Console.WriteLine("Errou! ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
