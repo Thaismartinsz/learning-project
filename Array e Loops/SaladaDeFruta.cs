ad4d1da24066afee72d8db83362ea43c5f08f97f
thaismartinsz <thaismtinocotmgmail.com>
2022-03-20 20:25:33 -0300

    Salada de Frutas

---
+++ Array e Loops/SaladaDeFruta.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SaladaDeFruta
+{
+    class SaladaDeFruta
+    {
+        static void Main(string[] args)
+        {
+            string[] fruta = new string[5];
+            
+            Console.WriteLine("Escolha 5 frutas: ");
+
+            for (int i = 0; i < fruta.Length; i++)
+            {
+                fruta[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("A lista das suas frutas: ");
+
+            for (int i = 0; i < fruta.Length; i++)
+            {
+                Console.WriteLine("A fruta " + (i+1) + " é "+ fruta[i]);
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
