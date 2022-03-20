b58159f4f2563d006ec3ed6d8660935760d21f03
thaismartinsz <thaismtinocotmgmail.com>
2022-03-20 20:51:21 -0300

    Potencia

---
+++ Array e Loops/Potencia.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LearningProject            
+{
+    class Potencia
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos número legais você quer: ");
+
+            int count = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= count; i++)
+            {
+                double result = Math.Pow(2, i);
+                Console.WriteLine(result);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
