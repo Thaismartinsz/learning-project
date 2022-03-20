dd05858b66c93dd0b94a28a9b5362407ed348bfb
thaismartinsz <thaismtinocotmgmail.com>
2022-03-20 20:38:02 -0300

    Dice

---
+++ Array e Loops/Dice.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LearningProject
+{
+    class Dice
+    {
+        static void Main(string[] args)
+        {
+            Random numberGen = new Random();
+
+            int roll = 0;
+            int attempts = 0;
+
+            Console.WriteLine("press enter to roll the die.");
+
+            while (roll != 6)
+            {
+                Console.ReadKey();
+
+                roll = numberGen.Next(1, 7);
+                Console.WriteLine("You Rolled: " + roll);
+                attempts++;
+            }
+
+            Console.WriteLine("it took you" + attempts + " attempts to roll a six");
+
+            Console.ReadKey();
+        }
+    }
+}
