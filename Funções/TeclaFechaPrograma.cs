d42c1c9ef033564d1130638187f5199044084487
thaismartinsz <thaismtinocotmgmail.com>
2022-03-20 20:22:38 -0300

    Tecla Fecha Programa

---
+++ Funções/TeclaFechaPrograma.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LearningProject
+{
+    class TeclaFechaPrograma
+    {
+        static void Main(string[] args)
+        {
+            EsperarTeclaParaFecharPrograma();
+        
+            Console.ReadKey(true);
+        }
+
+        static void EsperarTeclaParaFecharPrograma()
+        {
+            Console.WriteLine("Aperte uma tecla para fechar o programa. ");
+            Console.ReadKey(true);
+            Console.WriteLine("Tem certeza?");
+
+            ConsoleKey teclaQueOUsuarioApertou = Console.ReadKey(true).Key;
+
+            if (teclaQueOUsuarioApertou == ConsoleKey.S)
+            {
+                Environment.Exit(0);
+            }
+
+            else if (teclaQueOUsuarioApertou == ConsoleKey.N)
+            {
+                Console.WriteLine("Obrigada e até breve. ");
+            }
+        }
+    }
+}
