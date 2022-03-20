a68b330f70d66d1072f0c888c6f136d7e845d171
thaismartinsz <thaismtinocotmgmail.com>
2022-03-20 20:11:35 -0300

    Tipos Fundamentais

---
+++ Tipos Fundamentais/TiposFundamentais.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LearningProject
+{
+    class TiposFundamentais
+    {
+        static void Main(string[] args)
+        {
+            int numero = 8;
+            Console.WriteLine(numero);
+
+            string frase = "Olá, me chamo Thais.";
+            Console.WriteLine(frase);
+
+            float altura = 1.64f;
+            Console.WriteLine(altura);
+
+            bool brasileira = true;
+            Console.WriteLine("Eu sou brasileira?");
+            Console.WriteLine(brasileira);
+           
+            Console.ReadKey();
+        }
+    }
+}
