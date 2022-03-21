2c38979dfd61da2861cdf969e44532b1eda0ae43
thaismartinsz <thaismtinocotmgmail.com>
2022-03-20 21:01:37 -0300

    Escrever For

---
+++ Funções/EscreverFor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LearningProject
+{
+    class EscreverFor
+    {
+        static void Main(string[] args)
+        {
+            string frase = "I love programing.";
+            Console.WriteLine(frase);
+    
+            string[] palavras = frase.Split(' ');
+            int totalPalavras = palavras.Length;
+
+            EscreverForNoConsole(palavras);
+          
+            Console.WriteLine($"Essa frase tem {totalPalavras} palavras.");
+
+            string fruta = "Banana";
+            string[] saladaDeFruta = new string[] { "Banana", "Maçã", "Morango" };
+
+            EscreverForNoConsole(saladaDeFruta);
+
+            Console.ReadKey();
+        }
+   
+        static void EscreverForNoConsole(string[] arrayDeString)
+        {
+            for (int i = 0; i < arrayDeString.Length; i++)
+            {
+                Console.WriteLine(arrayDeString[i]);
+            }
+
+            Console.WriteLine("Aleluia!");
+        }
+    }
+}
